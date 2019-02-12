1c4ae0fbc9bea81f47cd2ace69d3c9c8d9af3dd6
Jordan Sinou <sinoujordan@gmail.com>
2019-02-12 17:10:59 +0100

    Début cards slide 5

--- assets/js/index.js
+++ assets/js/index.js
@@ -61,7 +61,7 @@ var swiper = new Swiper('.swiper-container', {
   keyboard: {
     enabled: true
   },
-  initialSlide: 3,
+  initialSlide: 4,
   allowTouchMove: false
 });
 function toggleMenu() {
@@ -198,4 +198,33 @@ function changeFamily(elt) {
 function changeColor(elt) {
   var colorValue = elt.value;
   document.querySelector('textarea').style.color = '#' + colorValue;
+}
+function card() {
+  var cards = [
+    {img: "img/imgCard/logoMCFC.svg", title: "Twitter", description: "Gotham a remplacé Helvetica sur votre réseau social préféré le 30 mai 2014"},
+    {img: "img/imgCard/logoMCFC.svg", title: "Moonlight Movie Poster", description: "Gotham a été utilisée pour faire les affiches sur célèbre film Moonlight racontant l'histoire d'un jeune afro américain essayant de vivre son homosexualité."},
+    {img: "img/imgCard/logoMCFC.svg", title: "Arte Magazin", description: "Gotham est devenue la police utilisée par Arte. Ils l'utilisent essentiellement en capitales. Les graisses light, book et bold sont utilisées."},
+    {img: "img/imgCard/logoMCFC.svg", title: "James Bond Spectre Movie Poster", description: "Gotham a été utilisée sur certaines affiches du film James Bond Spectre. La police a essentiellement servi à écrire le mot \"Spectre\" en lettres capitales."},
+    {img: "img/imgCard/logoMCFC.svg", title: "Obama 2008", description: "Barack Obama a utilisé Gotham pour sa campagne présidentielle en 2008. On reconnaîtra notamment la très célèbre affiche \"HOPE\" conçue par Shepard Fairey."},
+    {img: "img/imgCard/logoMCFC.svg", title: "Starbucks Coffee", description: "Starbucks a utilisé Gotham pour la packaging de plusieurs de ses produits. La police est utilisée principalement pour des sous-titres qui décrivent le produit."},
+    {img: "img/imgCard/logoMCFC.svg", title: "Netflix", description: "Netflix utilisait Gotham avant de créer sa propre police pour des raisons financières"},
+    {img: "img/imgCard/logoMCFC.svg", title: "Spotify", description: "Le logo Spotify a été créé avec la typo Gotham avec un point rond et une ligature entre le f et le y"},
+    {img: "img/imgCard/logoMCFC.svg", title: "Manchester City écusson", description: "Nouveau logo dévoilé en décembre 2015 visant à moderniser l’écusson du club tout en se rapprochant de l’écusson « classique »"},
+    {img: "img/imgCard/logoMCFC.svg", title: "GQ Magazine", description: "GQ était le commanditaire de la police Gotham, elle a été créée pour ce Magazine en 2000 par Tobias Frere-Jones"}
+  ];
+  var slide5 = document.querySelector('.swiper-slide[data-ref=\'5\']');
+
+  for (var i = 0; i < cards.length; i++) {
+    var contentHTML = '<div class="card" index='+i+'><div class="row nm"><ion-icon onclick="closeCard(this)" name="close"></ion-icon><div class="col s12 np"><img src='+cards[i].img+'></div></div><div class="row nm"><div class="col s12"><h3>'+cards[i].title+'</h3></div></div><div class="row nm"><div class="col s12"><p>'+cards[i].description+'</p></div></div></div>'
+    slide5.insertAdjacentHTML('beforeend', contentHTML)
+  }
+}
+function openCard(elt) {
+  var indexCard = elt.getAttribute('data-num');
+  var cards = document.getElementsByClassName('card');
+
+  addClass(cards[indexCard], 'open');
+}
+function closeCard(elt) {
+  removeClass(elt.parentNode.parentNode, 'open')
 }
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,11 +1,11 @@
 function removeClass(el, className){
-  document.querySelector(el).className = document.querySelector(el).className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
+  el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
 }
 function addClass(el, className){
-  if (!hasClass(el, className)) document.querySelector(el).className += ' ' + className;
+  if (!hasClass(el, className)) el.className += ' ' + className;
 }
 function hasClass(el, className){
-  return new RegExp('\\b'+ className+'\\b').test(document.querySelector(el).className);
+  return new RegExp('\\b'+ className+'\\b').test(el.className);
 }
 function toggleClass(el, className){
   hasClass(el, className) ? removeClass(el,className) : addClass(el,className);
