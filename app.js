7f8ae273acee74f40bd8b100da48ca7758acd72b
Jordan Sinou <sinoujordan@gmail.com>
2023-04-23 21:24:00 +0200

    delete comment

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -9,13 +8,8 @@ const appDir = path.dirname(require.main.filename);
 app.set('view engine', 'pug');
 app.use(express.static(appDir + "/assets"));
 
-// app.use(bodyParser.urlencoded({extended: false}));
-
-//Routes
 const index = require('./routes/index.js');
 app.use('/', index);
-// const adminRoutes = require('./routes/admin');
-// app.use('/admin', adminRoutes)
 
 const port = process.env.PORT || 3300;
 app.listen(port, () => console.log(`Serveur lancé sur le port ${port}`));
